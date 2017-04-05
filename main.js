f541e6ead335b603178374f558a1f16d18914c69
Luna <luna.huerta.c@gmail.com>
2017-04-05 20:59:47 -0300

    se agregan cambios

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ var nombre = prompt("Hola, ¿como te llamas?")
 var edad = prompt("¿Cuántos años tienes?")
 var sueño = prompt("¿Qué edad te gustaría tener en realidad?")
 
-var faltan = Number(sueño)-Number(edad)
-var dream = Number(edad)-Number(sueño)
+var faltan = Number(sueño) - Number(edad)
+var dream = Number(edad) - Number(sueño)
 
 if (edad < sueño){
-  alert("Naaa, te faltan "+faltan+" años!")
+  alert("Naaa, te faltan " + faltan + " años!")
 }
 else{
-  alert("Hahaha, tendrías que rejuvenecer "+dream+" años!")
+  alert("Hahaha, tendrías que rejuvenecer " + dream + " años!")
 }
 
 
