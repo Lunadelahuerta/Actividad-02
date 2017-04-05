af5fd3aa91c63e48d3f28a43999a1fe7cf0602db
Luna <luna.huerta.c@gmail.com>
2017-04-05 20:54:28 -0300

    finaliza actividad

---
+++ main.js
@@ -0,0 +1,15 @@
+var nombre = prompt("Hola, ¿como te llamas?")
+var edad = prompt("¿Cuántos años tienes?")
+var sueño = prompt("¿Qué edad te gustaría tener en realidad?")
+
+var faltan = Number(sueño)-Number(edad)
+var dream = Number(edad)-Number(sueño)
+
+if (edad < sueño){
+  alert("Naaa, te faltan "+faltan+" años!")
+}
+else{
+  alert("Hahaha, tendrías que rejuvenecer "+dream+" años!")
+}
+
+
